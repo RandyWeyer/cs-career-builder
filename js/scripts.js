47aae0a5ad1eabdc395125b7dd8b2ba1a770b654
Randy Weyer <RandyPWeyer@gmail.com>
2018-03-23 14:03:06 -0700

    add evaluation of language preferences in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,117 @@ $(document).ready(function() {
 
         event.preventDefault();
 
-    var frontBackEnd = $('input[name=front-back-end]:checked').val();
+        var computerOS = $('input[name=computer-os]:checked').val();
+        var frontBackEnd = $('input[name=front-back-end]:checked').val();
+        var webApp = $('input[name=web-app]:checked').val();
+        var mobileDesktop = $('input[name=mobile-desktop]:checked').val();
+        var preferDatabase = $('input[name=prefer-database]:checked').val();
 
-    
+        var preferJavascript = 0;
+        var preferPhp = 0;
+        var preferRuby = 0;
+        var preferCSharp = 0;
+        var preferPython = 0;
+        // values are added or subtracted by 0..3 depending on answers
 
+        var unfinishedQuiz = false;
+
+
+        if (computerOS==apple) {
+          preferJavascript+=2;
+          preferPhp+=1;
+          preferRuby+=2;
+          preferPython-=2;
+          preferCSharp-=3;
+        } else if (computerOS==microsoft) {
+          preferJavascript+=3;
+          preferPhp-=2;
+          preferRuby-=3;
+          preferPython-=2;
+          preferCSharp+=3;
+        } else if (computerOS==linux) {
+          preferJavascript+=1;
+          preferPhp+=3;
+          preferRuby+=3;
+          preferPython+=2;
+          preferCSharp-=1;
+        } else {
+          unfinishedQuiz = true;
+        }
+
+        if (frontBackEnd==frontend) {
+          preferJavascript+=3;
+          preferPhp-=3;
+          preferRuby-=1;
+          preferPython-=1;
+          preferCSharp+=1;
+        } else if (frontBackEnd==backend) {
+          preferJavascript-=3;
+          preferPhp+=1;
+          preferRuby+=3;
+          preferPython+=2;
+          preferCSharp-=1;
+        } else {
+          unfinishedQuiz = true;
+        }
+
+        if (webApp==web) {
+          preferJavascript+=3;
+          preferPhp+=2;
+          preferRuby+=3;
+          preferPython-=2;
+          preferCSharp-=4;
+        } else if (webApp==app) {
+          preferJavascript-=1;
+          preferPhp-=3;
+          preferRuby-=3;
+          preferPython+=2;
+          preferCSharp+=3;
+        } else if (webApp==both) {
+          preferJavascript+=3;
+          preferPhp-=3;
+          preferRuby+=3;
+          preferPython+=2;
+          preferCSharp+=1;
+        } else {
+          unfinishedQuiz = true;
+        }
+
+        if (mobileDesktop==mobile) {
+          preferJavascript+=3;
+          preferPhp-=3;
+          preferRuby-=2;
+          preferPython-=1;
+          preferCSharp+=1;
+        } else if (mobileDesktop==desktop) {
+          preferJavascript+=3;
+          preferPhp+=1;
+          preferRuby+=3;
+          preferPython+=2;
+          preferCSharp-=1;
+        } else {
+          unfinishedQuiz = true;
+        }
+
+        if (preferDatabase==database) {
+          preferJavascript+=1;
+          preferPhp+=1;
+          preferRuby+=3;
+          preferPython-=1;
+          preferCSharp+=1;
+        } else if (preferDatabase==noDatabase) {
+          preferJavascript+=3;
+          preferPhp-=3;
+          preferRuby-=3;
+          preferPython+=2;
+          preferCSharp+=1;
+        } else {
+          unfinishedQuiz = true;
+        }
+
+        if (unfinishedQuiz==true) {
+          alert("Please finish all fields before submitting.")
+        }
   });
 
   // Popover Bootstrap.js
@@ -27,7 +134,7 @@ $(document).ready(function() {
     $("#ruby-info").append("<a href='#' title='Ruby' data-toggle='popover' data-trigger='focus' data-content='Ruby INFO'>Ruby</a>"
   )};
 
-  function javascriptInfo() {
+  function pythonInfo() {
     $("#python-info").append("<a href='#' title='Python' data-toggle='popover' data-trigger='focus' data-content='Python INFO'>Python</a>"
   )};
 
